b63fdbb3cbc98a4eb27a3fda0d6d6c56daf066a6
Ozancann01 <22008306@student.hhs.nl>
2025-10-26 23:47:50 +0100

    traingle added

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
+
 import pygame
 from constants import *
+from player import Player    # Correct class import
+
 def main():
-    
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
     pygame.init()
-    
     clock = pygame.time.Clock()
     dt = 0
-
-    screen=pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
-    
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-
+        player.draw(screen)
         pygame.display.flip()
         clock.tick(60)
-        dt=clock.get_time()/1000
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        dt = clock.get_time() / 1000
 
 if __name__ == "__main__":
     main()
+
